2c0e28e4d40d305a1e048c89ceb77ea88124cddf
girvenavery2022 <girvenavery@gmail.com>
2022-03-25 12:31:13 -0400

    initial commit

---
+++ include/ros2_sick/LMS1xx_node.hpp
@@ -0,0 +1,91 @@
+// MIT License
+//
+// Copyright (c) 2022 Avery Girven
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include <memory>
+#include <chrono>
+
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/header.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "sensor_msgs/msg/imu.hpp"
+
+#include <ros2_sick/LMS1xx/LMS1xx.h>
+#include <tf2/exceptions.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include "string"
+
+namespace Sick 
+{
+class Sick : public rclcpp::Node 
+{
+public:
+    explicit Sick(rclcpp::NodeOptions options);
+
+    /**
+    * @brief connect to the Sick LMS1xx lidar
+    */
+    void connect_lidar();
+
+private:
+
+    // laser data
+    LMS1xx laser;
+    scanCfg cfg;
+    scanOutputRange outputRange;
+    scanDataCfg dataCfg;
+
+    // parameters
+    std::string host{};
+    std::string frame_id{};
+    int port{2112};
+    bool tf_correction{};
+    sensor_msgs::msg::LaserScan scan_msg;
+
+    // publishers
+    rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr ls_publisher_;
+
+    /**
+    * @brief construct a scan message based on the
+    * configuration of the sick lidar
+    * @param scan sensor_msgs::msg::laserscan
+    */
+    void construct_scan();
+
+    /**
+    * @brief get measurements from the lidar after it has 
+    * been setup properly
+    */
+    void get_measurements();
+
+    /**
+    * @brief publishes scan messages
+    */
+    void publish_scan();
+
+};
+
+}
---
+++ src/LMS1xx_node.cpp
@@ -0,0 +1,173 @@
+#include "ros2_sick/LMS1xx/LMS1xx.h"
+#include "ros2_sick/LMS1xx_node.hpp"
+#include <memory>
+#include <functional>
+
+#define DEG2RAD M_PI/180.0 // TODO get rid of this!!
+
+namespace Sick
+{
+Sick::Sick(rclcpp::NodeOptions options)
+: Node("sick_node", options)
+{
+  host = this->declare_parameter("host", "192.168.1.100");
+  frame_id = this->declare_parameter("frame_id", "laser_link");
+  port = this->declare_parameter("port", 2111);
+  tf_correction = this->declare_parameter("tf_correction", true);
+
+  ls_publisher_ = this->create_publisher<sensor_msgs::msg::LaserScan>(
+    "/sick/scan", 10);
+}
+
+void Sick::connect_lidar()
+{
+  RCLCPP_INFO(this->get_logger(), "Connecting to Lidar");
+  laser.connect(host, port);
+
+  if (!laser.isConnected())
+  {
+    RCLCPP_ERROR(this->get_logger(), "Unable to connect, retrying.");
+    return;
+  }
+
+  RCLCPP_INFO(this->get_logger(), "Logging in to laser.");
+  laser.login();
+  cfg = laser.getScanCfg();
+  outputRange = laser.getScanOutputRange();
+
+  RCLCPP_INFO(this->get_logger(),"Connected to laser.");
+  construct_scan();
+  get_measurements();
+}
+
+void Sick::construct_scan()
+{
+  scan_msg.header.frame_id = frame_id;
+  scan_msg.range_min = 0.01;
+  scan_msg.range_max = 20.0;
+  scan_msg.scan_time = 100.0 / cfg.scaningFrequency;
+  scan_msg.angle_increment = ((double)outputRange.angleResolution / 2) / 10000.0 * DEG2RAD;
+  if(tf_correction) 
+  {
+    scan_msg.angle_min = ((((double)cfg.startAngle) / 10000.0) - 90.0) * DEG2RAD;
+    scan_msg.angle_max = ((((double)cfg.stopAngle) / 10000.0) - 90.0) * DEG2RAD;
+  } 
+  else 
+  {
+    scan_msg.angle_min = ((double)cfg.startAngle ) / 10000.0 * DEG2RAD;
+    scan_msg.angle_max = ((double)cfg.stopAngle ) / 10000.0 * DEG2RAD;
+  }
+
+  //int angle_range = outputRange.stopAngle - outputRange.startAngle;
+  int num_values;
+
+  if (cfg.angleResolution == 2500)
+  {
+    num_values = 1081;
+  }
+  else if (cfg.angleResolution == 5000)
+  {
+    num_values = 541;
+  }
+  else
+  {
+    RCLCPP_ERROR(this->get_logger(), "Unsupported resolution");
+    return;
+  }
+
+  scan_msg.ranges.resize(num_values);
+  scan_msg.intensities.resize(num_values);
+
+  scan_msg.time_increment =
+    (outputRange.angleResolution / 10000.0)
+    / 360.0
+    / (cfg.scaningFrequency / 100.0);
+}
+
+void Sick::get_measurements()
+{
+  dataCfg.outputChannel = 1;
+  dataCfg.outputChannel = 1;
+  dataCfg.remission = true;
+  dataCfg.resolution = 1;
+  dataCfg.encoder = 0;
+  dataCfg.position = false;
+  dataCfg.deviceName = false;
+  dataCfg.outputInterval = 1;
+
+  RCLCPP_INFO(this->get_logger(), "Setting scan data configuration.");
+  laser.setScanDataCfg(dataCfg);
+
+  RCLCPP_INFO(this->get_logger(),"Setting scan data configuration.");
+  laser.startMeas();
+
+  RCLCPP_INFO(this->get_logger(),"Waiting for ready status.");
+  rclcpp::Time ready_status_timeout = this->get_clock()->now() + rclcpp::Duration::from_nanoseconds(5000);
+
+  status_t stat = laser.queryStatus();
+  rclcpp::Duration::from_nanoseconds(1000);
+  if (stat != ready_for_measurement)
+  {
+    RCLCPP_ERROR(this->get_logger(),"Laser not ready. Retrying initialization.");
+    laser.disconnect();
+    return;
+  }
+
+  RCLCPP_INFO(this->get_logger(), "Starting device.");
+  laser.startDevice(); // Log out to properly re-enable system after config
+
+  RCLCPP_INFO(this->get_logger(),"Commanding continuous measurements.");
+  laser.scanContinous(1);
+
+  while (rclcpp::ok())
+  {
+    scan_msg.header.stamp = this->get_clock()->now();
+
+    scanData data;
+    RCLCPP_INFO(this->get_logger(),"Reading scan data.");
+    if (laser.getScanData(&data))
+    {
+      for (int i = 0; i < data.dist_len1; i++)
+      {
+        scan_msg.ranges[i] = data.dist1[i] * 0.001;
+      }
+
+      for (int i = 0; i < data.rssi_len1; i++)
+      {
+        scan_msg.intensities[i] = data.rssi1[i];
+      }
+
+      RCLCPP_INFO(this->get_logger(),"Publishing scan data.");
+      publish_scan();
+    }
+    else
+    {
+      RCLCPP_ERROR(this->get_logger(),"Laser timed out on delivering scan, attempting to reinitialize.");
+      break;
+    }
+  }
+  laser.scanContinous(0);
+  laser.stopMeas();
+  laser.disconnect();
+}
+
+
+void Sick::publish_scan()
+{
+  ls_publisher_->publish(scan_msg);
+}
+
+} // namespace sick
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc,argv);
+  rclcpp::executors::SingleThreadedExecutor exec;
+  rclcpp::NodeOptions options;
+  auto lp_node = std::make_shared<Sick::Sick>(options);
+  exec.add_node(lp_node);
+  lp_node->connect_lidar();
+  exec.spin();
+  rclcpp::shutdown();
+  return 0;
+}
